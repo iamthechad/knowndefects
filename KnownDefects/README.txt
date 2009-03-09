1247c8d3e8150dfd1cd4c454976bd02285c7bd89
Chad Johnston <cjohnston@megatome.com>
2009-03-09 17:58:20 +0000

    Initial Commit

---
+++ KnownDefects/README.txt
@@ -0,0 +1,62 @@
+KnownDefects Utility
+---------------------
+
+Annotation Usage:
+* Add kd.jar to your classpath.
+* Annotate methods containing known defects with @KnownDefect("brief description of defect")
+
+Collecting Annotations:
+* Install FindBugs (http://findbugs.sourceforge.net/)
+* Add kd_findbugs.jar to the plugin directory of your FindBug install. (You may have to look for this directory in your Eclipse directory structure if you have the Eclipse plugin installed.)
+* Run FindBugs as normal. KnownDefect annotations are collected under the "Correctness" category.
+
+-------------------------------------------------------------
+
+Why Would I Want To Use This Annotation?
+
+I wrote this annotation to help me when working with legacy code. Much of the code I work with does not have associated unit tests, so its behavior is not well documented.
+When adding functionality or changing an existing part of the system, I try to write as many unit tests as I can to document the behavior and to help ensure that I don't break existing functionality.
+
+In a perfect world, the legacy code would all be correct and the unit tests would be an accurate description of the system's behavior.
+Since I don't live in a perfect world, the reality is that quite often the legacy code contains bugs. By writing tests that document the behavior of the system, I am also writing tests that pass because of these bugs.
+I don't want someone else to look at the tests as an accurate description of how the system works, especially if the behavior is dependent on defects.
+The answer I went with was to create an annotation that can be added to (preferably test) methods to indicate that the behavior of the method is dependent on a known defect.
+
+Here's an example from the code that prompted me to create this annotation:
+
+public void testValidateNullResponse() {
+   try {
+      ProtocolParser.validateResponse(null);
+      fail();
+   } catch (InvalidRequestException expected) {
+      // Caught expected exception
+   }
+}
+
+This test documents the system as it currently exists, but the behavior is obviously not correct. The ProtocolParser class should be throwing an InvalidResponseException, not an InvalidRequestException.
+To document this defect, I add the following annotation to the test method:
+
+@KnownDefect("Should throw InvalidResponseException")
+
+The correct behavior is now documented, and the test still passes.
+
+The FindBugs detector was added as a way to collect the KnownDefect instances so that defects can get properly handled and not lost in the code.
+
+--------------------------------------------------------------------------
+
+/*
+ * Copyright 2009 Megatome Technologies
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+  
---
+++ KnownDefects/src/org/megatome/knowndefect/findbugs/AbstractAnnotationDetector.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Megatome Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.megatome.knowndefect.findbugs;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.ba.ClassContext;
+import edu.umd.cs.findbugs.bcel.AnnotationDetector;
+import edu.umd.cs.findbugs.bcel.BCELUtil;
+import edu.umd.cs.findbugs.classfile.MethodDescriptor;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+
+import java.util.Map;
+
+/**
+ * Abstract base class for KnownDefect and KnownAndAcceptedDefect detectors
+ *
+ * @author cjohnston
+ */
+public abstract class AbstractAnnotationDetector extends AnnotationDetector {
+
+    protected final BugReporter bugReporter;
+    private String currentClass;
+    private Method currentMethod;
+
+    /**
+     * Create an instance of this detector
+     *
+     * @param bugReporter BugReporter
+     */
+    public AbstractAnnotationDetector(final BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
+
+    @Override
+    public void visit(final JavaClass someObj) {
+        this.currentClass = someObj.getClassName();
+        super.visit(someObj);
+    }
+
+    @Override
+    public void visit(final Method method) {
+        this.currentMethod = method;
+        super.visit(method);
+    }
+
+    @Override
+    public void visitAnnotation(String annotationClass,
+                                Map<String, Object> map, boolean runtimeVisible) {
+        super.visitAnnotation(annotationClass, map, runtimeVisible);
+        if (!getAnnotationClass().equals(annotationClass)) {
+            return;
+        }
+        final MethodDescriptor md = new MethodDescriptor(this.currentClass,
+                this.currentMethod.getName(),
+                this.currentMethod.getSignature(), this.currentMethod
+                        .isStatic());
+        final BugInstance bi = new BugInstance(this, getBugType(), NORMAL_PRIORITY)
+                .addClassAndMethod(md);
+        this.bugReporter.reportBug(bi);
+    }
+
+    @Override
+    public void visitClassContext(final ClassContext classContext) {
+        final JavaClass javaClass = classContext.getJavaClass();
+        if (!BCELUtil.preTiger(javaClass))
+            javaClass.accept(this);
+    }
+
+    @Override
+    public void report() {
+        // Not sure if this is necessary...
+        this.currentMethod = null;
+    }
+
+    /**
+     * Get the annotation class to look for
+     *
+     * @return Fully qualified class name
+     */
+    public abstract String getAnnotationClass();
+
+    /**
+     * Get the bug type to use when reporting. Defined in messages.xml
+     *
+     * @return Bug Type
+     */
+    public abstract String getBugType();
+}
